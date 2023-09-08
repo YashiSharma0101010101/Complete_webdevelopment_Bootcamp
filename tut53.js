496ee9790ebe5f4de7dcf2707bfe502c7a432e50
yash123674884 <yashisharma0039@gmail.com>
2023-09-09 03:36:43 +0530

    commit

---
+++ tut53.js
@@ -0,0 +1,27 @@
+console.log("This is a Tutorial 53");
+function greet (name,greetText){
+    console.log(name+" is a good boy");
+    console.log(greetText +" "+ name);
+}
+
+    
+
+let name="Harry";
+let name1="Shubham";
+let name2="Rohan";
+let name3="Sagar";
+let greetText="Good Morning";
+//greet(name);//function calling
+//greet(name1);//function calling
+//greet(name2);//function calling
+//greet(name3);//function calling
+greet(name,greetText);
+greet(name1,greetTtext);
+greet(name2,greetText);
+greet(name3,greetText);
+
+//simple method to print this type of information.
+//console.log(name + " is a good boy"); 
+//console.log(name1 + " is a good boy"); 
+//console.log(name2 + " is a good boy"); 
+//console.log(name3 + " is a good boy");  
